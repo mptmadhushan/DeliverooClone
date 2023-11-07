e35ec247008f141accaa17607e338d3dd0e0d9b6
tharindu_107365 <tharindu.madhushan@axiatadigitallabs.com>
2023-11-07 16:14:47 +0530

    resturent info cmp0

--- src/components/RestaurantInfo.tsx
+++ src/components/RestaurantInfo.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { FONTS, SIZES, COLORS } from '../constants/theme';
+import {View, Text, TouchableOpacity, Image,StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import {FONTS, SIZES, COLORS} from '../constants/theme';
 
 interface RestaurantInfoProps {
   restaurantName: string;
@@ -17,12 +17,61 @@ const RestaurantInfo: React.FC<RestaurantInfoProps> = ({
     <Text style={styles.restaurantName}>{restaurantName}</Text>
     <Text style={styles.cuisineType}>{cuisineType}</Text>
     <Text style={styles.details}>{details}</Text>
+    <View style={styles.infoContainer}>
+      <TouchableOpacity>
+        <Icon name="info-circle" color="black" size={30} />
+      </TouchableOpacity>
+
+      <View style={{width: SIZES.width * 0.6}}>
+        <Text style={styles.details}>Info</Text>
+        <Text>Map, allergens and hygiene rating</Text>
+      </View>
+      <TouchableOpacity>
+        <Icon name="reply" color="black" size={30} />
+      </TouchableOpacity>
+    </View>
+    <View style={styles.infoContainer}>
+      <TouchableOpacity>
+        <Icon name="star" color="black" size={30} />
+      </TouchableOpacity>
+
+      <View style={{width: SIZES.width * 0.6}}>
+        <Text style={[styles.details, {color: '#4d7c1b'}]}>4.8 Excellent</Text>
+        <Text>See all 500 reviews</Text>
+      </View>
+      <TouchableOpacity>
+        <Icon name="reply" color="black" size={30} />
+      </TouchableOpacity>
+    </View>
+    <View style={styles.infoContainer}>
+      <TouchableOpacity>
+      <Image
+        source={require('../../assets/rider.png')} // Use the imported image as the source
+        style={{ width: 40, height:40 }}
+      />
+      </TouchableOpacity>
+
+      <View style={{width: SIZES.width * 0.6}}>
+        <Text style={styles.details}>Deliver</Text>
+      </View>
+      <TouchableOpacity>
+      <Text style={[ {color:COLORS.primary,fontSize:20}]}>Change</Text>
+
+      </TouchableOpacity>
+    </View>
   </View>
 );
 
 const styles = StyleSheet.create({
   container: {
     padding: 20,
+  },
+  infoContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    height: 60,
+    alignItems: 'center',
   },
   restaurantName: {
     ...FONTS.h1,
