867f21a7b63dbf635512fae3a391d428ba70b642
tharindu_107365 <tharindu.madhushan@axiatadigitallabs.com>
2023-11-08 10:23:45 +0530

    splash screen added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {Platform} from 'react-native';
 import {Provider} from 'react-redux';
+import SplashScreen from 'react-native-splash-screen';
+
 import store from './Store/store';
 import AppNavigator from './navigation/AppNavigator';
 
 const App = () => {
+  useEffect(() => {
+    if (Platform.OS === 'android') SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <AppNavigator />
--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {useSelector} from 'react-redux';
-import SplashScreen from '../screens/SplashScreen';
+import {selectIsAuthenticated} from '../Store/slices/authSlice';
 import FoodList from '../screens/FoodList';
 import LoginScreen from '../screens/LoginScreen';
 import Home from '../screens/Home';
-import {selectIsAuthenticated} from '../Store/slices/authSlice';
+
 const Stack = createStackNavigator();
 
 export type RootStackParamList = {
-  Splash: undefined;
+  Home: undefined;
   FoodList: undefined;
   LoginScreen: undefined;
 };
@@ -21,11 +21,6 @@ function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Splash"
-          component={SplashScreen}
-          options={{headerShown: false}}
-        />
         <Stack.Screen
           name="Home"
           component={Home}
